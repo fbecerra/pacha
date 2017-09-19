74323c21ca482e59ce9e3bf1bebaef5147ccefa6
fbecerra <becerrafernando@gmail.com>
2017-09-19 15:51:11 -0400

    Added label for new fields

--- src/utils.py
+++ src/utils.py
@@ -121,6 +121,12 @@ def get_label(field):
     label = r'$v_{\rm rot}/v_{\rm kep}$'
   elif field == 'mach':
     label = r'$\mathcal{M}$'
+  elif field == 'angmom':
+    label == r'$l\,[?]'
+  elif field == 'taugrav':
+    label == r'$\tau_{\rm grav}\,[?]'
+  elif field == 'taupres':
+    label == r'$\tau_{\rm pres}\,[?]'
   elif field == 'nh':
     label = r'${\rm log}\left(n_{\rm H}/{\rm cm}^{-3}\right)$'
   elif field == 'dnh':
